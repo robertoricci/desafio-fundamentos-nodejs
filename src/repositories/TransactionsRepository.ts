23dfc2e0c2d7fdf93a905486bb3ecd7ceff323d2
Carlos Ricci <roberto.ricci@sonepar.com.br>
2020-04-18 14:30:20 -0300

    end project

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (accumulator, transaction) =>
+        transaction.type === 'income'
+          ? accumulator + transaction.value
+          : accumulator,
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (accumulator, transaction) =>
+        transaction.type === 'outcome'
+          ? accumulator + transaction.value
+          : accumulator,
+      0,
+    );
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
